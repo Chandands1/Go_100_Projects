6c3525b4b39b50f175cf2bde7cff42e557842190
chandan_ds <chandandhanush273747@gmail.com>
2026-03-13 08:55:07 +0530

    student project

--- Go_Projects/Student-Marks-Management/main.go
+++ Go_Projects/Student-Marks-Management/main.go
@@ -1,35 +1,92 @@
-package mai
+package main
 
-import ("fmt")
+import (
+	"fmt"
+)
 
-type Student struct{
-	Name string
+type Student struct {
+	Name       string
 	RollNumber string
-	Marks float64
-	Total float64
-	Average float64
-	Grade rune
+	Total      float64
+	Average    float64
+	Grade      rune
 }
 
-func startStudent(){
+func startStudent() {
 	std1 := createStudent()
-
-
+	enterMarks(&std1)
 }
-func createStudent(std *Student){
 
-	fmt.Print("Enter the student name:")
+func createStudent() Student {
+	var std Student
+
+	fmt.Print("Enter the student name: ")
 	fmt.Scanln(&std.Name)
-	fmt.Print("Enter the Roll number:")
-	fmt.Scanln(&std.RollNumber)
-	fmt.Print("Enter the Total:")
-	fmt.Scanln(&std.Total)
 
+	fmt.Print("Enter the Roll number: ")
+	fmt.Scanln(&std.RollNumber)
+
+	return std
+}
+
+func enterMarks(std *Student) {
+	var math, science, social float64
+
+	fmt.Println("Enter your marks for each subject")
+
+	fmt.Print("Enter Math marks: ")
+	fmt.Scanln(&math)
+
+	fmt.Print("Enter Science marks: ")
+	fmt.Scanln(&science)
+
+	fmt.Print("Enter Social marks: ")
+	fmt.Scanln(&social)
+
+	std.Total = calculateTotal(math, science, social)
+
+	std.Average = calculateAverage(std.Total)
+
+	std.Grade = calculateGrade(std.Average)
+
+	displayResult(std)
+}
+
+func calculateTotal(math, science, social float64) float64 {
+	return math + science + social
+}
+
+func calculateAverage(total float64) float64 {
+	return total / 3
+}
+
+func calculateGrade(average float64) rune {
+
+	if average >= 90 {
+		return 'A'
+	} else if average >= 80 {
+		return 'B'
+	} else if average >= 70 {
+		return 'C'
+	} else if average >= 60 {
+		return 'D'
+	} else {
+		return 'F'
+	}
 
 }
 
+func displayResult(std *Student) {
 
-func main(){
+	fmt.Println("\n----- STUDENT RESULT -----")
+	fmt.Println("Name       :", std.Name)
+	fmt.Println("RollNumber :", std.RollNumber)
+	fmt.Println("Total Marks:", std.Total)
+	fmt.Println("Average    :", std.Average)
+	fmt.Println("Grade      :", string(std.Grade))
+
+}
+
+func main() {
 	startStudent()
-
 }
